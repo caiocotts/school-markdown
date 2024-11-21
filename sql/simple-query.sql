fdb1bdf16030915370766cf067f476e044491f47
Caio Cotts <caio@cotts.com.br>
2024-11-21 11:46:45 -0500

    fix didn't actually push my finised work for some reason I submitted an sql file with one query only

--- sql/simple-query.sql
+++ sql/simple-query.sql
@@ -1,3 +1,28 @@
+
+-- Q1
+SELECT title, due_date
+FROM assignments 
+WHERE course_id IS 'COMP1234'
+
+-- Q2
+SELECT min(due_date)
+FROM assignments 
+
+-- Q3
+SELECT max(due_date)
+FROM assignments 
+
+-- Q4
+SELECT title, course_id
+FROM assignments 
+WHERE due_date Is '2024-10-08'
+
+-- Q5
 SELECT title, due_date 
 FROM assignments 
 WHERE due_date LIKE '2024-10%'
+
+-- Q6
+SELECT title, max(due_date)
+From assignments
+WHERE status is 'Completed'
